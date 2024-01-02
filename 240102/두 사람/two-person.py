f529ff59855c6197ea7a6a7e7bbc34cc95b51103
Sangwoo Jin <61958748+Jin-s-work@users.noreply.github.com>
2024-01-02 15:50:00 +0900

    Title: 두 사람, Time: 143ms, Memory: 24MB, Status: Passed - CodeTree

--- 240102/두 사람/two-person.py
+++ 240102/두 사람/two-person.py
@@ -2,7 +2,7 @@ a,b = map(str, input().split())
 
 c, d = map(str, input().split())
 
-if (int(a) >= 19 and b == 'M') or (int(c) >= 19 and d == 'W'):
+if (int(a) >= 19 and b == 'M') or (int(c) >= 19 and d == 'M'):
     print("1")
 else:
     print("0")
