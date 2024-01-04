f642cfab43ff35c7c1cdbcb10c179a35e72f556d
Sangwoo Jin <61958748+Jin-s-work@users.noreply.github.com>
2024-01-04 14:47:59 +0900

    Title: 소수 판별기, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240104/소수 판별기/prime-number-discriminator.py
@@ -0,0 +1,13 @@
+import math
+n = int(input())
+
+check = False
+
+for i in range(2, int(math.sqrt(n) + 1)):
+    if n % i == 0:
+        check = True
+
+if check:
+    print("C")
+else:
+    print("P")
