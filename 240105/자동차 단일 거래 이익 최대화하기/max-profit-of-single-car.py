297751f58f781259666d062ab814e5b3a2518d8b
Sangwoo Jin <61958748+Jin-s-work@users.noreply.github.com>
2024-01-05 18:16:24 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 223ms, Memory: 73MB, Status: Passed - CodeTree

--- 240105/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
+++ 240105/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
@@ -7,7 +7,7 @@ for i in range(len(arr)):
     for j in range(i+1, len(arr)):
         arr2.append(arr[j] - arr[i])
 
-if len(arr2) == 0:
+if len(arr2) == 0 or max(arr2) < 0:
     print(0)
 else:
     print(max(arr2))
