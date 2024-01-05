9c0b48d0e184f1d9136d53d432a8e38a4c8975d1
Sangwoo Jin <61958748+Jin-s-work@users.noreply.github.com>
2024-01-06 01:46:57 +0900

    Title: 문자열의 특정 위치 찾기 2, Time: 134ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240106/문자열의 특정 위치 찾기 2/find-specific-location-in-spring-2.py
@@ -0,0 +1,11 @@
+arr = ['apple', 'banana', 'grape', 'blueberry', 'orange']
+
+n = input()
+
+cnt = 0
+for i in range(len(arr)):
+    if (n == arr[i][2] or n == arr[i][3]):
+        print(arr[i])
+        cnt += 1
+
+print(cnt)
