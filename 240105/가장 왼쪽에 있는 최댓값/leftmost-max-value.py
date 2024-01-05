e8ce50bf7e25778edf8ec94057ce89ade93c0b49
Sangwoo Jin <61958748+Jin-s-work@users.noreply.github.com>
2024-01-05 17:57:45 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 147ms, Memory: 73MB, Status: Passed - CodeTree

--- 240105/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
+++ 240105/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
@@ -2,17 +2,13 @@ n = int(input())
 
 arr = list(map(int, input().split()))
 
+arr2 = []
+Max = -1
+for i in range(len(arr)):
+    if arr[i] > Max:
+        Max = arr[i]
+        idx = i
+        arr2.append(idx+1)
 
-while True:
-    idx = 0
-    Max = -1
-    for i in range(len(arr)):
-        if arr[i] > Max:
-            Max = arr[i]
-            idx = i
-    print(idx+1, end = " ")
-
-    if len(arr) == 1 or len(set(arr)) == 1:
-        break
-
-    arr = arr[:idx]
+for i in range(len(arr2)-1, -1, -1):
+    print(arr2[i], end = " ")
