8945ee880a6b31a745675db68b5fbf3f2a50f57c
Sangwoo Jin <61958748+Jin-s-work@users.noreply.github.com>
2024-01-07 16:40:30 +0900

    Title: 문자열 놀이, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240107/문자열 놀이/play-with-string.py
+++ 240107/문자열 놀이/play-with-string.py
@@ -3,17 +3,30 @@ s, q = map(str, input().split())
 for i in range(int(q)):
     k, a, b = map(str, input().split())
     
-    if int(k) == 1:
-        a = int(a)
-        b = int(b)
-        print(s[:a-1] + s[b-1] + s[a:b-1] + s[a-1] + s[b:])
-        s = s[:a-1] + s[b-1] + s[a:b-1] + s[a-1] + s[b:]
-    elif int(k) == 2:
-        ans = ""
-        for st in s:
-            if st == a:
-                ans += b
-            else:
-                ans += st
-        print(ans)
-        s = ans
+    if k == '1':
+        a, b = int(a), int(b)
+        s_list = list(s)
+        s_list[a-1], s_list[b-1] = s_list[b-1], s_list[a-1]
+        s = ''.join(s_list)
+        print(s)
+    elif k == '2':
+        s = s.replace(a, b)
+        print(s)
+        
+    # if int(k) == 1:
+    #     a = int(a)
+    #     b = int(b)
+    #     ans = s[:a-1] + s[b-1] + s[a:b-1] + s[a-1] + s[b:]
+    #     print(ans)
+    #     s = ans
+    # elif int(k) == 2 and len(a) == 1:
+    #     ans = ""
+    #     ans = ''.join([b if char == a else char for char in s])
+
+        # for st in s:
+        #     if st == a:
+        #         ans += b
+        #     else:
+        #         ans += st
+        # print(ans)
+        # s = ans
