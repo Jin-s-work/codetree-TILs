6fdae707e2c3d660d9346288944224625c406e84
Sangwoo Jin <61958748+Jin-s-work@users.noreply.github.com>
2024-01-07 16:15:36 +0900

    Title: 특정 문자의 등장 횟수, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240107/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
+++ 240107/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
@@ -1,3 +1,9 @@
 s = input()
+a, b = 0,0
+for i in range(len(s)):
+    if s[i:i+2] == 'ee':
+        a += 1
+    if s[i:i+2] == 'eb':
+        b += 1
 
-print(s.count('ee'), s.count('eb'))
+print(a, b)
